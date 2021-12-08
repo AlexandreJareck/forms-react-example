7f3a71f58e1a8a70937f018beea64ccaeeefb9a6
Alexandre Jareck <xandysjp@gmail.com>
2021-12-08 17:59:28 -0300

    styles in auth

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,7 @@
+import Auth from 'templates/Auth'
+
+const SignIn = () => {
+  return <Auth title="Sign In">Text</Auth>
+}
+
+export default SignIn
---
+++ src/templates/Auth/index.tsx
@@ -0,0 +1,48 @@
+import Heading from 'components/shared/Heading'
+import Logo from 'components/shared/Logo'
+import {
+  BannerBlock,
+  BannerContent,
+  Container,
+  ContentWrapper,
+  Subtitle,
+  Footer,
+  Content
+} from './styles'
+
+export type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
+  <Container>
+    <BannerBlock>
+      <BannerContent>
+        <Logo id="banner" />
+
+        <div>
+          <Heading>All your favorite games in one place</Heading>
+          <Subtitle>
+            <strong>WON</strong> is the best and most complete gaming platform.
+          </Subtitle>
+        </div>
+
+        <Footer>Won Games 2020 © Todos os Direitos Reservados</Footer>
+      </BannerContent>
+    </BannerBlock>
+
+    <Content>
+      <ContentWrapper>
+        <Logo id="content" color="black" size="large" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
+
+        {children}
+      </ContentWrapper>
+    </Content>
+  </Container>
+)
+
+export default Auth
