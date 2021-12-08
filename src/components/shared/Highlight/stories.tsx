745d23bcc238f9d14f26e91f26d3062dcb49b7d0
Alexandre Jareck <xandysjp@gmail.com>
2021-12-08 14:55:46 -0300

    create home page

--- src/components/shared/Highlight/stories.tsx
+++ src/components/shared/Highlight/stories.tsx
@@ -1,16 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
+import items from './mock'
 
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead it’s back',
-    subtitle: 'Come see John’s new adventures',
-    buttonLabel: 'Buy now',
-    backgroundImage: '/img/red-dead-img.jpg',
-    buttonLink: '/games/rdr2'
-  }
+  args: { ...items }
 } as Meta
 
 export const Default: Story<HighlightProps> = (args) => (
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,94 @@
+import Footer from 'components/Footer'
+import Menu from 'components/Menu'
+import { BannerProps } from 'components/shared/Banner'
+import BannerSlider from 'components/shared/BannerSlider'
+import { Container } from 'components/shared/Container'
+import { GameCardProps } from 'components/shared/GameCard'
+import GameCardSlider from 'components/shared/GameCardSlider'
+import Heading from 'components/shared/Heading'
+import Highlight, { HighlightProps } from 'components/shared/Highlight'
+import {
+  SectionBanner,
+  SectionFooter,
+  SectionFreeGames,
+  SectionMostPopular,
+  SectionNews,
+  SectionUpcoming
+} from './styles'
+
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcommingGames: GameCardProps[]
+  upcommingHighligth: HighlightProps
+  upcommingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighligth: HighlightProps
+}
+
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingGames,
+  upcommingHighligth,
+  upcommingMoreGames,
+  freeGames,
+  freeHighligth
+}: HomeTemplateProps) => (
+  <section>
+    <Container>
+      <Menu />
+      <SectionBanner>
+        <BannerSlider items={banners} />
+      </SectionBanner>
+    </Container>
+
+    <SectionNews>
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
+        </Heading>
+        <GameCardSlider items={newGames} color="black" />
+      </Container>
+    </SectionNews>
+
+    <Container>
+      <SectionMostPopular>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
+        </Heading>
+        <Highlight {...mostPopularHighlight} />
+        <GameCardSlider items={mostPopularGames} />
+      </SectionMostPopular>
+    </Container>
+
+    <SectionUpcoming>
+      <Heading lineLeft lineColor="secondary">
+        Upcomming
+      </Heading>
+      <GameCardSlider items={upcommingGames} />
+      <Highlight {...upcommingHighligth} />
+      <GameCardSlider items={upcommingMoreGames} />
+    </SectionUpcoming>
+
+    <SectionFreeGames>
+      <Heading lineLeft lineColor="secondary">
+        Free Games
+      </Heading>
+      <Highlight {...freeHighligth} />
+      <GameCardSlider items={freeGames} />
+    </SectionFreeGames>
+
+    <SectionFooter>
+      <Container>
+        <Footer />
+      </Container>
+    </SectionFooter>
+  </section>
+)
+
+export default Home
