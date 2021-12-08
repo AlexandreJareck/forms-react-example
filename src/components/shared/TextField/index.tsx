d9534cf72cb2bfeee698244f3bf466d84248898f
Alexandre Jareck <xandysjp@gmail.com>
2021-12-08 17:05:24 -0300

    create text field with error

---
+++ src/components/shared/TextField/index.tsx
@@ -0,0 +1,54 @@
+import { useState, InputHTMLAttributes } from 'react'
+import { Container, Icon, Input, InputWrapper, Label, Error } from './styles'
+
+export type TextFieldProps = {
+  onInput?: (value: string) => void
+  label?: string
+  labelFor?: string
+  initialValue?: string
+  icon?: React.ReactNode
+  iconPosition: 'left' | 'right'
+  disabled?: boolean
+  error?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+const TextField = ({
+  label,
+  labelFor = '',
+  error,
+  initialValue = '',
+  onInput,
+  icon,
+  iconPosition = 'left',
+  disabled = false,
+  ...props
+}: TextFieldProps) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.value
+    setValue(newValue)
+
+    !!onInput && onInput(newValue)
+  }
+
+  return (
+    <Container disabled={disabled} error={!!error}>
+      {!!label && <Label htmlFor={labelFor}>{label}</Label>}
+      <InputWrapper>
+        {!!icon && <Icon iconPosition={iconPosition}>{icon}</Icon>}
+        <Input
+          type="text"
+          onChange={onChange}
+          value={value}
+          iconPosition={iconPosition}
+          disabled={disabled}
+          {...props}
+        />
+      </InputWrapper>
+      {!!error && <Error>{error}</Error>}
+    </Container>
+  )
+}
+
+export default TextField
