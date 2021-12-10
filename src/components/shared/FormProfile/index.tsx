ac15c60cbea546e917c1fe27082b70cf1301e2df
Alexandre Jareck <xandysjp@gmail.com>
2021-12-10 17:07:14 -0300

    create form profile

---
+++ src/components/shared/FormProfile/index.tsx
@@ -0,0 +1,48 @@
+import Heading from '../Heading'
+import TextField from '../TextField'
+import { Form } from './styles'
+import Button from 'components/shared/Button'
+
+const FormProfile = () => (
+  <>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+
+    <Form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        label="Name"
+        initialValue="John Doe"
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        initialValue="johndoe@gmail.com"
+        label="E-mail"
+        disabled
+      />
+
+      <TextField
+        name="password"
+        type="password"
+        placeholder="Type your password"
+        label="Password"
+      />
+
+      <TextField
+        name="new_password"
+        type="password"
+        placeholder="New password"
+        label="New password"
+      />
+
+      <Button size="large">Save</Button>
+    </Form>
+  </>
+)
+
+export default FormProfile
