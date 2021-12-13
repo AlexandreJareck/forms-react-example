7af753c517cc1ec7b0947b62820b88bd44e636d9
Alexandre Jareck <xandysjp@gmail.com>
2021-12-13 13:16:57 -0300

    create order list

--- src/components/shared/FormProfile/index.tsx
+++ src/components/shared/FormProfile/index.tsx
@@ -2,47 +2,50 @@ import Heading from '../Heading'
 import TextField from '../TextField'
 import { Form } from './styles'
 import Button from 'components/shared/Button'
+import { useEffect } from 'react'
 
-const FormProfile = () => (
-  <>
-    <Heading lineBottom color="black" size="small">
-      My profile
-    </Heading>
+const FormProfile = () => {
+  return (
+    <>
+      <Heading lineBottom color="black" size="small">
+        My profile
+      </Heading>
 
-    <Form>
-      <TextField
-        name="name"
-        placeholder="Name"
-        label="Name"
-        initialValue="John Doe"
-      />
+      <Form>
+        <TextField
+          name="name"
+          placeholder="Name"
+          label="Name"
+          initialValue="John Doe"
+        />
 
-      <TextField
-        name="email"
-        type="email"
-        placeholder="E-mail"
-        initialValue="johndoe@gmail.com"
-        label="E-mail"
-        disabled
-      />
+        <TextField
+          name="email"
+          type="email"
+          placeholder="E-mail"
+          initialValue="johndoe@gmail.com"
+          label="E-mail"
+          disabled
+        />
 
-      <TextField
-        name="password"
-        type="password"
-        placeholder="Type your password"
-        label="Password"
-      />
+        <TextField
+          name="password"
+          type="password"
+          placeholder="Type your password"
+          label="Password"
+        />
 
-      <TextField
-        name="new_password"
-        type="password"
-        placeholder="New password"
-        label="New password"
-      />
+        <TextField
+          name="new_password"
+          type="password"
+          placeholder="New password"
+          label="New password"
+        />
 
-      <Button size="large">Save</Button>
-    </Form>
-  </>
-)
+        <Button size="large">Save</Button>
+      </Form>
+    </>
+  )
+}
 
 export default FormProfile
