83f422f3a47ef3c14dfb6924782279fdc78ca0db
Alexandre Jareck <xandysjp@gmail.com>
2021-12-13 17:19:58 -0300

    adjust component cardlist

--- src/components/shared/CardList/index.tsx
+++ src/components/shared/CardList/index.tsx
@@ -1,19 +1,29 @@
 import GameItem, { GameItemProps } from 'components/shared/GameItem'
+import Link from 'next/link'
 import { Container, Footer, Total } from './styles'
+import Button from 'components/shared/Button'
 
 export type CartListProps = {
   items: GameItemProps[]
   total: string
+  hasButton?: boolean
 }
 
-const CardList = ({ items, total }: CartListProps) => (
+const CardList = ({ items, total, hasButton = false }: CartListProps) => (
   <Container>
     {items.map((item) => (
       <GameItem key={item.title} {...item} />
     ))}
 
     <Footer>
-      Total <Total>{total}</Total>
+      {!hasButton && <span>Total:</span>}
+      <Total>{total}</Total>
+
+      {hasButton && (
+        <Link href="/cart">
+          <Button as="a">Buy it now</Button>
+        </Link>
+      )}
     </Footer>
   </Container>
 )
